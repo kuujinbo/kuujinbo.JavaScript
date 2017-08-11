e803ec31c83477eaafe9dd25e13545691d798fc2
kuujinbo <kuujinbo@gmail.com>
2017-08-11 15:25:36 -0700

    remove magic strings; add tests

---
+++ src/kuujinbo.JavaScript.Tests/CheckboxGroupToggler.Tests.js
@@ -0,0 +1,126 @@
+/// <reference path="./../kuujinbo.JavaScript/CheckboxGroupToggler.js" />
+
+'use strict';
+
+describe('CheckboxGroupToggler', function () {
+    var toggler, clickEvent = null;
+    var groupSelectorId = 'checkbox-group';
+    var selector = '#' + groupSelectorId;
+
+    function getGroupElement() {
+        return document.querySelector(selector);
+    }
+
+    function getGroupCheckboxes() {
+        return getGroupElement().querySelectorAll('input[type=checkbox]');
+    }
+
+    beforeEach(function () {
+        setFixtures(
+            '<div id="' + groupSelectorId + '">'
+            + "<input type='checkbox' />"
+            + "<input type='checkbox' />"
+            + "<input type='checkbox' checked />"
+            + '</div>'
+        );
+        toggler = new CheckboxGroupToggler(selector);
+        clickEvent = {};
+    });
+
+    describe('CheckboxGroupToggler object', function () {
+        it('throws when CSS selector is not passed to new()', function () {
+            expect(function () { new CheckboxGroupToggler(); })
+                .toThrow(new CheckboxGroupToggler('x').initError);
+        });
+
+        it('initializes object when CSS selector passed to new()', function () {
+            expect(toggler).toBeDefined();
+            expect(toggler.getCheckAllHtml).toEqual(jasmine.any(Function));
+            expect(toggler.getUncheckAllHtml).toEqual(jasmine.any(Function));
+            expect(toggler.getCheckGroup).toEqual(jasmine.any(Function));
+            expect(toggler.addToggleElement).toEqual(jasmine.any(Function));
+            expect(toggler.clickToggleElement).toEqual(jasmine.any(Function));
+        });
+    });
+
+    describe('getCheckGroup', function () {
+        it('throws when CSS selector does not match', function () {
+            var badToggler = new CheckboxGroupToggler('#does-not-match');
+
+            expect(function () { badToggler.getCheckGroup(); })
+                .toThrow(toggler.containerError);
+        });
+
+        it('returns Element when CSS selector matches', function () {
+            var containerElement = toggler.getCheckGroup();
+
+            expect(containerElement).toEqual(jasmine.any(Element));
+        });
+    });
+
+    describe('addToggleElement', function () {
+        it('adds toggler after by default', function () {
+            spyOn(toggler, 'getCheckAllHtml');
+            spyOn(toggler, 'getCheckGroup').and.callThrough();
+
+            toggler.addToggleElement();
+            var toggleNode = getGroupElement().nextSibling;
+
+            expect(toggler.getCheckAllHtml).toHaveBeenCalledTimes(1);
+            expect(toggler.getCheckGroup).toHaveBeenCalledTimes(1);
+            expect(toggleNode.tagName).toBe('SPAN');
+            expect(toggleNode.children.length).toBe(1);
+            expect(toggleNode.children[0].tagName).toBe('BUTTON');
+            expect(toggleNode.children[0].className).toBe(toggler.buttonCheckClassList);
+        });
+
+        it('adds toggler before when explicitly called', function () {
+            spyOn(toggler, 'getCheckAllHtml');
+            spyOn(toggler, 'getCheckGroup').and.callThrough();
+
+            toggler.addToggleElement(true);
+            var toggleNode = getGroupElement().parentNode.firstElementChild;
+
+            expect(toggler.getCheckAllHtml).toHaveBeenCalledTimes(1);
+            expect(toggler.getCheckGroup).toHaveBeenCalledTimes(1);
+            expect(toggleNode.tagName).toBe('SPAN');
+            expect(toggleNode.children.length).toBe(1);
+            expect(toggleNode.children[0].tagName).toBe('BUTTON');
+            expect(toggleNode.children[0].className).toBe(toggler.buttonCheckClassList);
+        });
+
+        it('updates the button and checks all when button has [checked] class', function() {
+            spyOn(toggler, 'getUncheckAllHtml');
+            toggler.addToggleElement();
+            clickEvent.target = getGroupElement().nextSibling.firstChild;
+
+            toggler.clickToggleElement(clickEvent);
+            var checkboxes = getGroupElement().querySelectorAll('input[type="checkbox"]:checked');
+
+            expect(getGroupElement().nextSibling.firstChild.className).toBe(toggler.buttonUncheckClassList);
+            expect(toggler.getUncheckAllHtml).toHaveBeenCalledTimes(1);
+            expect(checkboxes.length).toBe(getGroupCheckboxes().length);
+        });
+
+
+        it('updates the button and unchecks all when button does NOT have [checked] class', function () {
+            spyOn(toggler, 'getCheckAllHtml');
+            toggler.addToggleElement();
+            var button = getGroupElement().nextSibling.firstChild;
+            button.classList.remove('checked');
+            clickEvent.target = button;
+
+            toggler.clickToggleElement(clickEvent);
+            var checkboxes = getGroupElement().querySelectorAll('input[type="checkbox"]:not(:checked)');
+
+            expect(getGroupElement().nextSibling.firstChild.className).toBe(toggler.buttonCheckClassList);
+            // called in addToggleElement() && clickToggleElement()
+            expect(toggler.getCheckAllHtml).toHaveBeenCalledTimes(2);
+            expect(checkboxes.length).toBe(getGroupCheckboxes().length);
+        });
+
+    });
+
+    describe('XXX', function() {
+    });
+});
--- src/kuujinbo.JavaScript/CheckboxGroupToggler.js
+++ src/kuujinbo.JavaScript/CheckboxGroupToggler.js
@@ -1,50 +1,65 @@
-function CheckboxGroupToggler() {
-    this._checkGroupSelector = '';
+function CheckboxGroupToggler(groupSelector) {
+    Object.defineProperty(this, 'initError', {
+        value: 'CSS selector for the checkbox group parent DOM element required.'
+    });
+
+    Object.defineProperty(this, 'containerError', {
+        value: 'checkbox group parent DOM element not found. verify CSS selector is correct'
+    });
+
+    Object.defineProperty(this, 'buttonUncheckClassList', {
+        value: 'btn btn-default'
+    });
+    Object.defineProperty(this, 'buttonCheckClassList', {
+        value: 'btn btn-primary checked'
+    });
+
+    if (!groupSelector || !groupSelector.match(/\w+/)) throw this.initError;
+
+    this._checkGroupSelector = groupSelector;
 }
 
 CheckboxGroupToggler.prototype = {
     constructor: CheckboxGroupToggler,
-    getCheckAllHtml: function () {
+    getCheckAllHtml: function() {
         return "<span class='glyphicon glyphicon-check'></span> Check All";
     },
-    getUncheckAllHtml: function () {
+    getUncheckAllHtml: function() {
         return "<span class='glyphicon glyphicon-unchecked'></span> Uncheck All";
     },
-    getCheckGroup: function () {
-        return document.querySelector(this._checkGroupSelector);
+    getCheckGroup: function() {
+        var element = document.querySelector(this._checkGroupSelector);
+        if (!element) throw this.containerError;
+
+        return element;
     },
-    setCheckGroupSelector: function (selector) {
-        this._checkGroupSelector = selector;
-    },
-    addToggleElement: function () {
+    addToggleElement: function(addBefore) {
+        var toggler = document.createElement('span');
+        toggler.innerHTML = "<button type='button' class='"
+            + this.buttonCheckClassList
+            + "' style='margin:8px 0;text-align:left;width:120px'>"
+            + this.getCheckAllHtml()
+            + '</button>';
         var checkGroup = this.getCheckGroup();
-        if (checkGroup !== null) {
-            var toggler = document.createElement('span');
-            toggler.innerHTML = "<button type='button' class='btn btn-primary checked' style='text-align:left;width:120px'>"
-                + this.getCheckAllHtml()
-                + '</button>';
-            toggler.addEventListener(
-                'click', this.clickToggleElement.bind(this), false
-            );
-            // add toggler **immediately** following target element
-            checkGroup.parentNode.insertBefore(toggler, checkGroup.nextSibling);
-        }
+        checkGroup.parentNode.insertBefore(toggler, addBefore ? checkGroup : checkGroup.nextSibling);
+
+        toggler.addEventListener(
+            'click', this.clickToggleElement.bind(this), false
+        );
     },
-    clickToggleElement: function (e) {
+    clickToggleElement: function(e) {
         var checkGroup = this.getCheckGroup();
-        if (checkGroup !== null) {
-            var toggler = e.target;
-            if (toggler.classList.contains('checked')) {
-                var elements = checkGroup.querySelectorAll('input[type="checkbox"]:not(:checked)');
-                for (i = 0; i < elements.length; ++i) elements[i].checked = true;
-                toggler.className = 'btn btn-default';
-                toggler.innerHTML = this.getUncheckAllHtml();
-            } else {
-                var elements = checkGroup.querySelectorAll('input[type="checkbox"]:checked');
-                for (i = 0; i < elements.length; ++i) elements[i].checked = false;
-                toggler.className = 'btn btn-primary checked';
-                toggler.innerHTML = this.getCheckAllHtml();
-            }
+        var toggler = e.target;
+        if (toggler.className === this.buttonCheckClassList) {
+            var elements = checkGroup.querySelectorAll('input[type="checkbox"]:not(:checked)');
+            for (i = 0; i < elements.length; ++i) elements[i].checked = true;
+            toggler.className = this.buttonUncheckClassList;
+            toggler.innerHTML = this.getUncheckAllHtml();
+        } else {
+            var elements = checkGroup.querySelectorAll('input[type="checkbox"]:checked');
+            for (i = 0; i < elements.length; ++i) elements[i].checked = false;
+            toggler.className = this.buttonCheckClassList;
+            toggler.innerHTML = this.getCheckAllHtml();
         }
     }
 }
